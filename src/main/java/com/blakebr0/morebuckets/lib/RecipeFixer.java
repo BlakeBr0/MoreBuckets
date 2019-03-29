8cadf82cb2b52c9d4326d9a175d3ba102d53c2f1
BlakeBr0 <luasmrks@gmail.com>
2019-03-28 19:35:58 -0500

    Fix recipes with bucket outputs getting modified

--- src/main/java/com/blakebr0/morebuckets/lib/RecipeFixer.java
+++ src/main/java/com/blakebr0/morebuckets/lib/RecipeFixer.java
@@ -1,20 +1,18 @@
 package com.blakebr0.morebuckets.lib;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-
 import com.blakebr0.morebuckets.MoreBuckets;
 import com.blakebr0.morebuckets.item.ItemMoreBucket;
-
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.UniversalBucket;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class RecipeFixer {
 	
@@ -24,6 +22,10 @@ public class RecipeFixer {
 //		ForgeRegistries.RECIPES.register(new ShapelessRecipes("wow", new ItemStack(Items.ACACIA_BOAT), NonNullList.<Ingredient>withSize(1, Ingredient.fromStacks(new ItemStack(Items.WATER_BUCKET)))).setRegistryName("testerino"));
 //		ForgeRegistries.RECIPES.register(new ShapelessOreRecipe(new ResourceLocation("hello", "there"), new ItemStack(Items.DARK_OAK_BOAT), new ItemStack(Items.LAVA_BUCKET)).setRegistryName("tester"));
 		ForgeRegistries.RECIPES.forEach(recipe -> {
+			ItemStack output = recipe.getRecipeOutput();
+			if (output.getItem() instanceof UniversalBucket || output.getItem() == Items.LAVA_BUCKET || output.getItem() == Items.WATER_BUCKET || output.getItem() == Items.MILK_BUCKET)
+				return;
+
 			NonNullList<Ingredient> ing = recipe.getIngredients();
 			boolean shapeless = false;
 			boolean shapelessOre = false;
