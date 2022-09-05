c11c0bbe92ba5804dfb0656429adcf6c8f6d2ce5
BlakeBr0 <luasmrks@gmail.com>
2022-09-05 17:17:47 -0500

    Create feature flags

---
+++ src/main/java/com/blakebr0/morebuckets/config/ModFeatureFlags.java
@@ -0,0 +1,9 @@
+package com.blakebr0.morebuckets.config;
+
+import com.blakebr0.cucumber.util.FeatureFlag;
+import com.blakebr0.morebuckets.MoreBuckets;
+import net.minecraft.resources.ResourceLocation;
+
+public final class ModFeatureFlags {
+    public static final FeatureFlag RECIPE_FIXER = FeatureFlag.create(new ResourceLocation(MoreBuckets.MOD_ID, "recipe_fixer"), ModConfigs.ENABLE_RECIPE_FIXER);
+}
