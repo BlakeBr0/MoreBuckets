6f1bbc359676f55c193fad5d8b10c807f1a96413
BlakeBr0 <luasmrks@gmail.com>
2022-09-06 17:58:54 -0500

    Add @FeatureFlags annotation to feature flags class

--- src/main/java/com/blakebr0/morebuckets/config/ModFeatureFlags.java
+++ src/main/java/com/blakebr0/morebuckets/config/ModFeatureFlags.java
@@ -1,9 +1,11 @@
 package com.blakebr0.morebuckets.config;
 
 import com.blakebr0.cucumber.util.FeatureFlag;
+import com.blakebr0.cucumber.util.FeatureFlags;
 import com.blakebr0.morebuckets.MoreBuckets;
 import net.minecraft.resources.ResourceLocation;
 
+@FeatureFlags
 public final class ModFeatureFlags {
     public static final FeatureFlag RECIPE_FIXER = FeatureFlag.create(new ResourceLocation(MoreBuckets.MOD_ID, "recipe_fixer"), ModConfigs.ENABLE_RECIPE_FIXER);
 }
