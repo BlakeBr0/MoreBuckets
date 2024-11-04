6712806e1e541038661609cd007ccb3eb02a70f5
BlakeBr0 <luasmrks@gmail.com>
2024-11-03 20:53:53 -0600

    Catch and log recipe fixer replacement errors

--- src/main/java/com/blakebr0/morebuckets/crafting/RecipeFixer.java
+++ src/main/java/com/blakebr0/morebuckets/crafting/RecipeFixer.java
@@ -1,6 +1,7 @@
 package com.blakebr0.morebuckets.crafting;
 
 import com.blakebr0.cucumber.helper.RecipeHelper;
+import com.blakebr0.morebuckets.MoreBuckets;
 import com.blakebr0.morebuckets.config.ModConfigs;
 import com.blakebr0.morebuckets.crafting.ingredient.FluidIngredient;
 import com.blakebr0.morebuckets.item.MoreBucketItem;
@@ -52,7 +53,12 @@ public class RecipeFixer implements ResourceManagerReloadListener {
                         for (var stack : value.getItems()) {
                             var item = stack.getItem();
                             if (item instanceof BucketItem || item instanceof MilkBucketItem || item instanceof IFluidHandler) {
-                                ingredients.set(i, new FluidIngredient(ingredient));
+                                try {
+                                    ingredients.set(i, new FluidIngredient(ingredient));
+                                } catch (Exception e) {
+                                    MoreBuckets.LOGGER.warn("Failed to modify ingredients for recipe {}, skipping", recipe.getId());
+                                }
+
                                 break;
                             }
                         }
